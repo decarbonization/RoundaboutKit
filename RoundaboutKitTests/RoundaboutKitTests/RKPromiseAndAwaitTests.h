d8b862a46d0b54db5238820798ee45549f0887c8
Kevin MacWhinnie <kevin@pinnaplayer.com>
2013-04-15 10:50:11 -0700

    Stubs for RKPromise and RKAwait.

---
+++ RoundaboutKitTests/RoundaboutKitTests/RKPromiseAndAwaitTests.h
@@ -0,0 +1,13 @@
+//
+//  RKPromiseAndAwaitTests.h
+//  RoundaboutKitTests
+//
+//  Created by Kevin MacWhinnie on 4/12/13.
+//
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface RKPromiseAndAwaitTests : SenTestCase
+
+@end
---
+++ RoundaboutKitTests/RoundaboutKitTests/RKPromiseAndAwaitTests.m
@@ -0,0 +1,25 @@
+//
+//  RKPromiseAndAwaitTests.m
+//  RoundaboutKitTests
+//
+//  Created by Kevin MacWhinnie on 4/12/13.
+//
+//
+
+#import "RKPromiseAndAwaitTests.h"
+
+@implementation RKPromiseAndAwaitTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+}
+
+- (void)tearDown
+{
+    [super tearDown];
+    
+}
+
+@end
