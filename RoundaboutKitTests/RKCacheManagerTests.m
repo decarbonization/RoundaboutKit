5fbe67d44e14f22b909b8daeff6ad0f12e4148e7
Kevin MacWhinnie <kevin@pinnaplayer.com>
2013-05-06 14:35:13 -0700

    More unscrewing up

---
+++ RoundaboutKitTests/RKCacheManagerTests.m
@@ -0,0 +1,13 @@
+//
+//  RKCacheManagerTests.m
+//  RoundaboutKitTests
+//
+//  Created by Kevin MacWhinnie on 5/6/13.
+//
+//
+
+#import "RKCacheManagerTests.h"
+
+@implementation RKCacheManagerTests
+
+@end
--- RoundaboutKitTests/RKURLRequestPromiseTests.m
+++ RoundaboutKitTests/RKURLRequestPromiseTests.m
@@ -9,35 +9,11 @@
 #import "RKURLRequestPromiseTests.h"
 #import "RKMockURLProtocol.h"
 
-@interface RKURLRequestPromiseTests ()
-
-@property (copy) NSArray *URLs;
-
-@end
-
 @implementation RKURLRequestPromiseTests
-
-- (NSArray *)predeterminedResponses
-{
-    NSURL *predeterminedResponsesLocation = [[NSBundle bundleForClass:[self class]] URLForResource:@"PredeterminedResponses" withExtension:@"plist"];
-    return [NSArray arrayWithContentsOfURL:predeterminedResponsesLocation];
-}
 
 - (void)registerPredeterminedResponses
 {
-    for (NSDictionary *predeterminedResponse in [self predeterminedResponses]) {
-        NSURL *url = [NSURL URLWithString:predeterminedResponse[@"URL"]];
-        NSString *method = predeterminedResponse[@"method"];
-        NSInteger statusCode = [predeterminedResponse[@"statusCode"] integerValue];
-        NSDictionary *headers = predeterminedResponse[@"headers"];
-        NSData *response = [predeterminedResponse[@"responseString"] dataUsingEncoding:NSUTF8StringEncoding];
-        
-        [RKMockURLProtocol on:url
-                   withMethod:method
-              yieldStatusCode:statusCode
-                      headers:headers
-                         data:response];
-    }
+    
 }
 
 - (void)setUp
